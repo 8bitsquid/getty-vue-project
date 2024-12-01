3441a6697874f9f3f7378f3a4f102e7dbfbce250
Will Jones <the8bitsquid@gmail.com>
2024-11-30 16:04:40 -0800

    initial commit

---
+++ src/components/QueryCollection.vue
@@ -0,0 +1,17 @@
+<script setup>
+	import { ref } from 'vue';
+
+	const defaultQuery = `SELECT * WHERE { 
+  ?sub ?pred ?obj . 
+} LIMIT 10`
+
+	const query = ref(defaultQuery);
+</script>
+
+<template>
+	 <h2>Query Getty Collections</h2>
+	<textarea v-model="query"></textarea>
+	<QueryResults 
+		:results="results"
+	/>
+</template>
---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<script setup>
+	import { ref } from 'vue';
+	import ToDoItem from './ToDoItem.vue';
+
+	const list = ref(["first", "second"," and now there's a third ToDo"]);
+</script>
+
+<template>
+	<h2>To Do's</h2>
+
+	<ToDoItem 
+		v-for="(item, index) in list"
+		:item="item"
+		:index="index"
+		:text="item" 
+		:key="index"
+	/>
+
+</template>
